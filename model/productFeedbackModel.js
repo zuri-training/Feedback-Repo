85d22aebcf346866c1397bbc085958d056ff4586
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-12-03 19:16:30 +0100

    created two feedback templates schema

---
+++ model/productFeedbackModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const ProductFeedbackTemplate = new mongoose.Schema({
+    Name: String,
+    Email: {
+        type: String,
+        required: true,
+    },
+    comment: String
+
+})
+
+
+module.exports = mongoose.model('ProductFeedbackTemplate', ProductFeedbackTemplate)
---
+++ model/serviceFeedbackTemplate.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const ServiceFeedbackTemplate = new mongoose.Schema ({
+    title: String,
+    description: String
+
+})
+
+
+
+
+
+module.exports = mongoose.model('ServiceFeedbackTemplate', ServiceFeedbackTemplate)
